60cf6ff262730041037f8ccfeeec69d5ff4e8d5b
Divya Shukla <divyashukla20993@gmail.com>
2026-04-26 12:43:50 +0530

    Added DOCKERFILE

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask 
-app = Flask(__name__) 
+app = Flask(__name__)
 @app.route("/") 
 def home(): 
     return "Hello from Python + pip! this is using FLASK" 
 if __name__ == "__main__": 
-    app.run(port=5000)
+    app.run(host="0.0.0.0", port=5000)
